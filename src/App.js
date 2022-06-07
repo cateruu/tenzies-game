099027f88dfa946f74148fd43ebc007cc49ca3fd
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-07 11:21:55 +0200

    Dies component with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import styles from './app.module.css';
+import Die from './components/Die';
 
 const App = () => {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <section className={styles.dies}>
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+        <Die />
+      </section>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/Die.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './die.module.css';
+
+const Die = () => {
+  return <div className={styles.die}>1</div>;
+};
+
+export default Die;
