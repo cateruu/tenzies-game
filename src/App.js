735b779aaae09f4e109a10069cd08be7fe25330a
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-07 11:48:05 +0200

    Naming correction

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import styles from './app.module.css';
 import Die from './components/Die';
 
 const App = () => {
+  const newDice = () => {
+    const arrayOfDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.ceil(Math.random() * 6);
+      arrayOfDice.push(randomNumber);
+    }
+
+    return arrayOfDice;
+  };
+
+  console.log(newDice());
+
   return (
     <main className={styles.main}>
-      <section className={styles.dies}>
+      <section className={styles.dice}>
         <Die value='1' />
         <Die value='1' />
         <Die value='1' />
