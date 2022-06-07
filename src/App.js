e5234146810c60e374d55892c661395c90f872b1
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-07 13:18:55 +0200

    change state of die on click

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import styles from './app.module.css';
 import Die from './components/Die';
 import RollBtn from './components/RollBtn';
 
 const App = () => {
-  const [dice, setDice] = useState(newDice());
-
-  const diceElements = dice.map((die) => <Die value={die} />);
-
-  function newDice() {
+  const newDice = () => {
     const arrayOfDice = [];
     for (let i = 0; i < 10; i++) {
       const randomNumber = Math.ceil(Math.random() * 6);
-      arrayOfDice.push(randomNumber);
+      arrayOfDice.push({
+        value: randomNumber,
+        isHeld: false,
+        id: nanoid(),
+      });
     }
 
     return arrayOfDice;
-  }
+  };
 
-  function rollDice() {
+  const rollDice = () => {
     setDice(newDice());
-  }
+  };
+
+  const holdDice = (id) => {
+    setDice((prevState) =>
+      prevState.map((die) => {
+        if (die.id === id) {
+          die.isHeld = !die.isHeld;
+        }
+
+        return die;
+      })
+    );
+  };
+
+  const [dice, setDice] = useState(newDice());
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      handleClick={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <main className={styles.main}>
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,14 @@ import React from 'react';
 import styles from './die.module.css';
 
 const Die = (props) => {
-  return <div className={styles.die}>{props.value}</div>;
+  return (
+    <div
+      className={`${styles.die} ${props.isHeld ? styles.held : ''}`}
+      onClick={props.handleClick}
+    >
+      {props.value}
+    </div>
+  );
 };
 
 export default Die;
